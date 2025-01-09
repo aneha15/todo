1f184284d76d68d205bd9c470c4cc03ea55034ba
neha1562005 <76821812+neha1562005@users.noreply.github.com>
2025-01-09 19:37:44 +0600

    Add project list

--- src/project.js
+++ src/project.js
@@ -1,6 +1,17 @@
 export default class Project {
+
     constructor(name, tasks = []) {
         this.name = name;
         this.tasks = tasks;
     }
+}
+
+export const projectList = {
+    personal: [],
+    work: [],
+    groceries: [],
+};
+
+function addToProjectList(name) {
+    projectList.push(name);
 }
