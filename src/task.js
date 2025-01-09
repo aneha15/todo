708d41939e0b94a485635ba4b9e73fea31a1c30c
neha1562005 <76821812+neha1562005@users.noreply.github.com>
2025-01-09 14:40:50 +0600

    Make task class

--- src/task.js
+++ src/task.js
@@ -1,4 +1,26 @@
 
 export default class Task {
-    
+    constructor(title, description, dueDate, priority, checked=false, project='personal') {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.checked = checked;
+        this.project = project;
+    }
+
+    setPriority(priority) {
+        this.priority = priority;
+    }
+
+    toggleChecked() {
+        this.checked = this.checked === false ? true : false;
+    }
+
+    setProject(project) {
+        this.project = project;
+    }
 }
+
+
+
